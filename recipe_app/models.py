d6883b0003ce78bfc01be291552eae5f58e587e8
gprophete <garlyprophete30@gmail.com>
2020-06-05 15:24:30 -0400

    models created

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=240)
+    image_url = models.TextField()
+    tags = models.CharField(max_length=255)
+
+class Ingredients(models.Model):
+    items = models.TextField()
+
+
+class Direction(models.Model):
+    steps = models.TextField()
+    prep_time = models.CharField(max_length=100)
+    servings = models.CharField(max_length=100)
+
+class Claps(models.Model): 
+
+
+ class Comments(models.Model):
+     content = models.TextField()
+     name = models.CharField(max_length=255)
+     email = models.CharField(max_length=255)
